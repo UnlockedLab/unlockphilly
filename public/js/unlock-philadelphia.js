01d00f3d75929c52cca8fb7b1d6522b18f86feba
James Tyack <jamestyack@gmail.com>
2014-05-10 17:20:50 -0400

    Improved Yelp popup rendering

--- public/js/unlock-philadelphia.js
+++ public/js/unlock-philadelphia.js
@@ -110,7 +110,7 @@ function addLayersAndShow(stationData, line) {
 					info.removeFrom(map);
 					infoVisible=false;
 				}
-				var zoom = Math.max(15, map.getZoom());
+				var zoom = Math.max(16, map.getZoom());
 				map.setView(new L.LatLng(lng, lat), zoom, {
 						animate: true,
 						});
@@ -133,8 +133,8 @@ function addLayerAndShowYelpResults(data, name) {
 				},
 				properties: {
 					title: alph.charAt(i) + ". <a target='_blank' href='" + business.url + "'>" + business.name + "</a>",
-					description: "<a target='_blank' href='" + business.url + "'><img style='max-width:80px' align='right' src='" + business.image_url + "'/></a><strong>" + business.categories[0][0] + "</strong><br />" + business.location.address + "<br />'Wheelchair Accessible'" +
-					      "<br/>More info in Yelp panel next to map, look for '" + alph.charAt(i) + "'",
+					description: "<a target='_blank' href='" + business.url + "'><img style='max-width:80px' align='right' src='" + business.image_url + "'/></a><strong>" + business.categories[0][0] + "</strong><br />" + business.location.address +
+					      "<br/><span><img title='" + business.snippet_text + "' src='" + business.rating_img_url + "'/></a><img src='http://s3-media1.ak.yelpcdn.com/assets/2/www/img/14f29ad24935/map/miniMapLogo.png' alt='Yelp Logo, mini' height='20' width='40'><br/><a target='_blank' href='" + business.url + "'>" + business.review_count + " reviews</span><br/>Yelp says 'Wheelchair Accessible', leave an accessibility review if you visit.",
 						'marker-size': 'small',
 						'marker-color': "#0099cc",
 						'marker-symbol': alph.toLowerCase().charAt(i)
@@ -167,7 +167,7 @@ function createListOfResults(data, name) {
 		resultsHtml += "<li class='list-group-item'><strong>";
 		resultsHtml += markerRef + ".</strong> <a target='_blank' href='" + business.url + "'>" + business.name + "</a> <strong>" + business.categories[0][0] +"</strong> (" +
 			 Math.round(business.distance) + " metres from " + name + ")<br />" + business.location.display_address[0] + " " + business.display_phone +
-			 " <br /><img title='" + business.snippet_text + "' src='" + business.rating_img_url + "'/></a> (" + business.review_count + " votes) ";
+			 " <br /><img title='" + business.snippet_text + "' src='" + business.rating_img_url + "'/></a> (" + business.review_count + " reviews) ";
 		resultsHtml += "<br />Yelp listing says 'Wheelchair Accessible'</li>";
 		$('#popoverData').popover();
 	}
