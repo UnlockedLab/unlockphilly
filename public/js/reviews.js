39f5e9d1d38ca1831dac01748dfa8f00c65baa28
James Tyack <jamestyack@gmail.com>
2014-10-10 07:08:49 -0400

    Clean up

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -24,32 +24,14 @@ function fetchData(apiAddress) {
         $("#loadingImage").hide();
           $.each(data.response.venues, function(i,venues){
             var contents = "";
-            $.ajax({
-              url: 'https://api.foursquare.com/v2/venues/'+venues.id+'/photos?&limit=1&oauth_token=ADKCZISL2BPWAUVWFY1EN4Z012FAPIYJQPYVLG1U4EXTCCZB&v=20140715',
-              dataType: 'json',
-              async: false,
-              cache: false,
-              success: function(data2) {
-                 var imgSrc = "";
-                 if (data2.response.photos.count > 0) {
-                     imgSrc = '<img src="' + data2.response.photos.items[0].prefix + '75x75' + data2.response.photos.items[0].suffix + '">';
-                 }
-                 else {
-                     imgSrc = '<img src="/images/no_image_available.gif" width="75">';
-                 }
-                 $('.results').append('<div class="col-xs-4 col-sm-2 col-md-2" style="margin-top:10px;padding-left:5px">' + imgSrc + '</div>');
-              }
-            });
-            
             contents += '<div class="col-xs-8 col-sm-8 col-md-10">';
-            contents += '<h4>' + venues.name + '</h4>';
+            contents += '<h4><a alt="Click to review accessibility" href="/postreviews/' + venues.id +'">' + venues.name + '</a></h4>';
             if(venues.location.address != null) contents += venues.location.address + ' ';
             if(venues.location.city != null) contents += venues.location.city + ' ';
             if(venues.location.state != null) contents += venues.location.state + ' ';
             if(venues.location.postalCode != null) contents += venues.location.postalCode + ' ';
             contents += '<br /><br />';
             
-            contents += '<button class="btn btn-primary"><a style="color:#fff" href="/postreviews/' + venues.id +'">Post Review</a></button>';
             contents += '</div>';
 
             contents += '<div style="clear:both"></div><br />';
